9c4925c30909cbd7473fccc5c7afc0a9f89b2c0e
ERTUĞRUL KUZGUN <kuzgunertugrul10@gmail.com>
2022-10-16 21:20:22 +0300

    some fixes

--- test/urls.py
+++ test/urls.py
@@ -1,7 +1,11 @@
+from unicodedata import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pentestivirzivir/login', views.login_page, name='login'),
+    path('pentestivirzivir/logout', views.logout_user, name='logout'),
+    path('pentestivirzivir/',views.pentestivirzivirView, name='pentestivirzivir')
 ]
--- test/views.py
+++ test/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from multiprocessing import context
+from urllib import request
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,'index.html')
 
+def login_page(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        print(form)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            print(username)
+            password = form.cleaned_data.get('password')
+            print(password)
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("index")
+            else:
+                return redirect('https://www.youtube.com/watch?v=RyK-A_dGbBw')
+        else:
+            messages.error(request,"bulaşma ulan")
+    return render(request,'login.html',context={'form':form})
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='/pentestivirzivir/login')
+def pentestivirzivirView(request):
+    return render(request,'pentestivirzivir.html')
 # Create your views here.
